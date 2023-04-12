59e893fe943d61ad0d8dd4eccd324ea50812dd6f
EgorKondratenko <122119909+EgorKondratenko@users.noreply.github.com>
2023-04-12 22:37:25 +0300

    Add files via upload

--- src/main/java/web/controller/CarController.java
+++ src/main/java/web/controller/CarController.java
@@ -1,24 +1,25 @@
 package web.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import web.model.Car;
-import web.service.CarCervice;
-import web.service.CarCerviceImp;
+import web.service.CarService;
 
-import java.util.List;
-
-import static web.model.Car.createListCars;
+import static web.service.CarServiceImp.listCar;
 
 @Controller
 public class CarController {
+
+    private CarService carService;
+    @Autowired
+    public CarController(CarService carService) {
+        this.carService = carService;
+    }
     @GetMapping("/cars")
     public String getCars(@RequestParam(value = "count", defaultValue = "5") int count, Model model) {
-        CarCervice carCervice = new CarCerviceImp();
-        List<Car> listCar = carCervice.getCarList(createListCars(), count);
+        listCar = carService.getCarList(count);
         model.addAttribute("cars", listCar);
         return "/cars";
     }
