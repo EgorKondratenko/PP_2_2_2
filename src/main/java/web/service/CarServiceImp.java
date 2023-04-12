a158c8a04a8dcc46b2f5d535b5d6c305a719a79c
EgorKondratenko <122119909+EgorKondratenko@users.noreply.github.com>
2023-04-12 22:38:17 +0300

    Add files via upload

--- src/main/java/web/service/CarServiceImp.java
+++ src/main/java/web/service/CarServiceImp.java
@@ -1,19 +1,22 @@
 package web.service;
 
+import org.springframework.stereotype.Component;
 import web.model.Car;
 
 import java.util.ArrayList;
 import java.util.List;
-
+@Component
 public class CarServiceImp implements CarService {
-    @Override
-    public List<Car> getCarList(List<Car> listCar, int count) {
-        listCar = new ArrayList<>();
+    public static List<Car> listCar = new ArrayList<>();
+    {
         listCar.add(new Car("Ford", "Focus", 2018));
         listCar.add(new Car("Nissan", "Almera", 2009));
         listCar.add(new Car("Hyundai", "Creta", 2020));
         listCar.add(new Car("Toyota", "Tundra", 2015));
         listCar.add(new Car("Kia", "Rio", 2019));
+    }
+    @Override
+    public List<Car> getCarList(int count) {
         return listCar.stream().limit(count).toList();
     }
 }
